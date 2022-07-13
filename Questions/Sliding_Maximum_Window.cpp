be532b5c8854615ac2aaa3a78a0274ca1d624e87
Neha2109 <porwalneha.porwal@gmail.com>
2022-07-13 11:31:18 +0530

    max sliding window

--- Questions/Sliding_Maximum_Window.cpp
+++ Questions/Sliding_Maximum_Window.cpp
@@ -1,2 +1,25 @@
+#include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
+
+int main(){
+
+    int n,k;
+    cin>>n>>k;
+    vector<int> arr(n);
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    vector<int>win;
+    for(int i=0;i<n-k;i++){
+        int ans=0;
+        for(int j=i;j<i+3;j++){
+              ans=max(ans,arr[j]);
+        }
+        win.push_back(ans);
+    }
+for(auto &i:win){
+        cout<<i<<"\t";
+    }
+
+}
