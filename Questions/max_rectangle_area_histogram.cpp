cbfe2f22e335f5d08b5eef4fe38da86656ee5d24
Neha2109 <porwalneha.porwal@gmail.com>
2022-08-18 18:42:53 +0530

    histogram

--- Questions/max_rectangle_area_histogram.cpp
+++ Questions/max_rectangle_area_histogram.cpp
@@ -1,11 +1,9 @@
+#include <bits/stdc++.h>
 #include<iostream>
 using namespace std;
 
 
-int max_area(vector<int> a){
 
-    
-}
 
 
 
@@ -14,6 +12,31 @@ int max_area(vector<int> a){
 
 
 int main(){
-    vector<int> a={2,1,5,6,2,3};
-    cout<<max_area(a);
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    int ans=0,i=0;
+stack <int> st;
+a.push_back(0);
+    while(i<n){
+        while(!st.empty() & a[st.top()]>a[i]){
+            int t=st.top();
+            int h=a[t];
+            st.pop();
+            if(st.empty()){
+                ans=max(ans,h*i);
+            }
+            else{
+                int len=i-st.top()-1;
+                ans=max(ans,h*len);
+            }
+
+        }
+        st.push(i);
+        i++;
+    }
+    cout<<ans;
 }
